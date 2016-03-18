0c9d6c6374d5f9d00231358a480ca1c85b3bfc35
trantrung2317 <nguyentrantrung2311@gmail.com>
2016-03-18 23:39:47 +0700

    something hihi

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :comments denpendent: destroy
+  has_many :comments
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence:true
 end
